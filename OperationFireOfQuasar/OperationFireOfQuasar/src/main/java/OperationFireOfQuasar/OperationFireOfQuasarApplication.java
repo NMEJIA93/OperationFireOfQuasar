acc5589fd6dfa6059f11e949c4ccf5296d957ba2
nmejia93 <norbeymejiacortes@gmail.com>
2024-11-23 10:31:26 -0500

    initial commit

---
+++ OperationFireOfQuasar/OperationFireOfQuasar/src/main/java/OperationFireOfQuasar/OperationFireOfQuasarApplication.java
@@ -0,0 +1,13 @@
+package OperationFireOfQuasar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationFireOfQuasarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperationFireOfQuasarApplication.class, args);
+	}
+
+}
